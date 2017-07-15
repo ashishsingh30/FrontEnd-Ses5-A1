958b78865f4e7bb5f2f6f0d198a119a4ea5339b0
ashishsingh30 <ashishksingh3004@gmail.com>
2017-07-15 16:25:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "Ashish";
+var age = "23";
+var DateOfBirth = "30/04/1994";
+var PlaceOfBirth = "Gandhinagar";
+console.log("Name : "+name);
+console.log("Age : "+age);
+console.log("Date Of Birth : "+DateOfBirth);
+console.log("Place Of Birth : "+PlaceOfBirth);
